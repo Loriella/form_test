8390c321ac9905a4f3c88b784f3e1faa835d1f19
Loriella <loris.orlova@gmail.com>
2020-03-04 15:58:42 +0300

    add DropModal component

---
+++ src/components/PageElements/DropModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DropModal = () => {
+  return (
+    <div className="drop-modal-wrap">
+      <div className="drop-modal">
+        <h3>Бросайте файлы сюда, я ловлю</h3>
+        <p>Мы принимаем картинки (jpg, png, gif), офисные файлы (doc, xls, pdf) и zip-архивы. Размеры файла до 5 МБ</p>
+      </div>
+    </div>
+  )
+};
+
+export default DropModal;
