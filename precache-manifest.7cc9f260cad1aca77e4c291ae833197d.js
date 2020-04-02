c6e9d7b89575d601cd828cc107d5c1c2248543f8
Loriella <loris.orlova@gmail.com>
2020-04-02 15:55:23 +0300

    Updates

---
+++ precache-manifest.7cc9f260cad1aca77e4c291ae833197d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8781e214f43d1fd1313d585416e7d7",
+    "url": "/form_test/index.html"
+  },
+  {
+    "revision": "798a6230c9d3cbb196a3",
+    "url": "/form_test/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cec7e933c16d4073ee4c",
+    "url": "/form_test/static/css/main.ffdb77a0.chunk.css"
+  },
+  {
+    "revision": "798a6230c9d3cbb196a3",
+    "url": "/form_test/static/js/2.144cb9b1.chunk.js"
+  },
+  {
+    "revision": "0bc5220d92bfe34c3dec2f124a7bb286",
+    "url": "/form_test/static/js/2.144cb9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec7e933c16d4073ee4c",
+    "url": "/form_test/static/js/main.fe762f4f.chunk.js"
+  },
+  {
+    "revision": "ee68f75883fe5ae5ed26",
+    "url": "/form_test/static/js/runtime-main.cc812ac9.js"
+  }
+]);
